01392493da12fe552942bbd4011ac15788b73339
nischal08 <nischal.karanjit@gmail.com>
2021-05-23 17:32:39 +0545

    implemented provider package for state management of app

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_drop/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -12,28 +13,42 @@ class ProductItem extends StatelessWidget {
   );
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.favorite,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
           ),
         ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.shopping_cart,
+        footer: GridTileBar(
+          leading: IconButton(
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+            ),
           ),
-        ),
-        backgroundColor: Colors.black54,
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+          trailing: IconButton(
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
